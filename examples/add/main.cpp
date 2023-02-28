2f8c58e46903231cea09b3731fadc81c1e32a541
Waken <votrevincentwaken@gmail.com>
2023-02-28 17:00:13 +0200

    Add CMake function to copy DLL and PDB to tests/examples binary directories when building for Windows

---
+++ examples/add/main.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include <set>
+#include <vector>
+#include <unordered_map>
+#include "Time.h"
+#include "Date.h"
+#include "Spendings.h"
+#include "MoneyStorage.h"
+#include "Card.h"
+#include "Wallet.h"
+#include "Interface.h"
+int main()
+{
+	Interface temp;
+	temp.Start();
+}
